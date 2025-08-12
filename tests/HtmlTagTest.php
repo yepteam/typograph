62d806f7560718d039ca7cf39fcd1d91e910bccd
Arthur Shlain <artur.slain@gmail.com>
2025-08-12 11:50:04 +0500

    Html Script Test

--- tests/HtmlTagTest.php
+++ tests/HtmlTagTest.php
@@ -192,4 +192,27 @@ final class HtmlTagTest extends TestCase
         $this->assertSame($html, $typograph->format($html));
     }
 
+    public function testScript()
+    {
+        $typograph = new Typograph(['entities' => 'named']);
+
+        $html = '<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(12345678, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/12345678" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->';
+        $this->assertSame($html, $typograph->format($html));
+    }
+
 }
